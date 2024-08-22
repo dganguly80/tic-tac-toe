3949e7992a88de13d85a4bdaf4858ecd7f632906
dganguly80 <dev_ece@yahoo.com>
2024-08-22 17:12:00 -0500

    updated version

--- src/TicTac.java
+++ src/TicTac.java
@@ -1,21 +1,24 @@
+//import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.*;
 public class TicTac {
 
-    public void finalResult(String winner){
+
+   /* public void finalResult(String winner){
         out.println("Winner is: " +  winner);
-    }
+    } */
 
     public static int[] checkWinner(String[] updatedGrid, int playerNumber){
         String playerChar;
-        int[] playerMovements=new int[updatedGrid.length];
+        int[] playerMovements={-1, -1, -1, -1, -1, -1, -1, -1, -1};
         if(playerNumber==1)
             {
                 playerChar="X";
             }
         else{
-            playerChar="O";
+                playerChar="O";
         }
         for(int i=0,m=0;i<updatedGrid.length;i++){
             if(updatedGrid[i].equals(playerChar)){
@@ -23,9 +26,8 @@ public class TicTac {
                 m++;
             }
         }
-        System.out.println(playerMovements);
+        System.out.println(Arrays.toString(playerMovements));
         return playerMovements;
-
     }
 
 //    public void player
@@ -48,6 +50,7 @@ public class TicTac {
     }
 
     public static String[] playerMove(int playerNum, String[] grid){
+
         Scanner tictacUser=new Scanner(in);
         out.println("Player " +playerNum+" make your move");
         //String myAnswer;
@@ -66,39 +69,62 @@ public class TicTac {
          return grid;
     }
 
+    public static boolean winChecker(int[] moveTracker, int[] winPattern){
+        int winFlag=0;
+        Arrays.sort(moveTracker);
+             for(int myMove=0,patternIndex=0;myMove<moveTracker.length;myMove++){
+                 if(moveTracker[myMove]==winPattern[patternIndex]){
+                        patternIndex++;
+                        winFlag++;
+                        if (winFlag==3){
+                            System.out.println("Winner");
+                            return true;
+                        }
+                 }
+             }
+             return false;
+    }
+
+
 
 
     public  static void main(String[] args) {
 
+        int[][] winPattern={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
         String[] gridTicTac = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
         int[] solution = new int[gridTicTac.length];
-        boolean gameFinish;
+        //int[] movement = new int[9];
         System.out.println("hello - Starting the tic-tac-toe game");
-//        boolean gameFinish=False;
-        //While(!gameFinish){
+
+        boolean gameFinish=false;
+        while(!gameFinish){
             gridTicTac = playerMove(1, gridTicTac);
+            int[] movement1= checkWinner(gridTicTac,1);
+            //check win for player 1
+            for(int j=0;j<8;j++){
+                boolean isResult=winChecker(movement1,winPattern[j]);
+                if(isResult){
+                    gameFinish=true;
+                    System.out.println("Winner is player 1");
+                    exit(0);
+                    break;
+                }
+            }
+
             gridTicTac = playerMove(2, gridTicTac);
-            gridTicTac = playerMove(1, gridTicTac);
-            solution= checkWinner(gridTicTac,1);
+            int[] movement2= checkWinner(gridTicTac,2);
+            //check Win for player 2
+            for(int j=0;j<8;j++){
+                boolean isResult=winChecker(movement2,winPattern[j]);
+                if(isResult){
+                    gameFinish=true;
+                    System.out.println("Winner is player 2");
+                    break;
+                }
+            }
 
-        //}
-
-
-        /*printCurrentGrid(gridTicTac);
-        Scanner tictacUser=new Scanner(System.in);
-        System.out.println("Player 1 make your move");
-        String myAnswer;
-        myAnswer=tictacUser.nextLine();
-        System.out.println(myAnswer);
-        gridTicTac[mapInput(myAnswer)]="X";
-        printCurrentGrid(gridTicTac);
-        System.out.println("Player 2 make your move");
-        myAnswer=tictacUser.nextLine();
-        System.out.println(myAnswer);
-        gridTicTac[mapInput(myAnswer)]="O";
-        printCurrentGrid(gridTicTac);
-        */
+        }
 
     }
 
